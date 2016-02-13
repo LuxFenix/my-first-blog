bae2be85aaf51a7e728a4f7125ef999ba178e2cf
lucia <lucia.pampana9888@gmail.com>
2016-02-13 12:51:45 +0000

    wsgisetup

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -20,4 +20,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-application = DjangoWhiteNoise(get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
