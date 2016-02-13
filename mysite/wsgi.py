a64e80dc92636f8a8aad57953432bb54c65b1465
lucia <lucia.pampana9888@gmail.com>
2016-02-13 12:48:44 +0000

    wsgisetup

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = 'home/LuxFenix/my-first-blog'
+if path not in sys.path:
+	sys.path.append(path)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(get_wsgi_application()
